8cbe5df1a72cd4b99219cf6e949d11b6d201ec04
milad asghari <miladasghari2016@gmail.com>
2019-05-20 23:41:47 +0430

    tunning

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './styles/index.css';
 import App from './components/App';
 import { ApolloProvider } from 'react-apollo'
 import { ApolloClient } from 'apollo-client'
